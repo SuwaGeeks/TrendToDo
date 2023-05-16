23ccfcacf1e6b47d2b0dfe0539e16e7a8990a1f9
Gbear <shimadachihiro5107@gmail.com>
2023-05-17 02:17:52 +0900

    add ナビゲーション

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import { Login } from "./components/Lgoin";
 import { TheHeader } from "./components/TheHeader";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <TheHeader />
       <Login />
+      <Navigation />
     </div>
   );
 }
