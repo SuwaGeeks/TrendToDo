bd189be78fd1da8975a4c73aaea05bc7e0b3c7cd
mochi-yu <mochi.2225.it@gmail.com>
2023-06-17 16:34:02 +0900

    feat: PersonalTaskListPageを作成 #13

---
+++ src/pages/PersonalTaskListPage.jsx
@@ -0,0 +1,25 @@
+import { Stack, Typography } from "@mui/material"
+import { Task } from "@mui/icons-material"
+
+import { PersonalTaskList } from "../components/PersonalTaskList"
+import { Footer } from "../components/Footer"
+
+export const PersonalTaskListPage = () => {
+  return (
+    <>
+      <Stack spacing={2} sx={{ px: "20px", py: "40px" }}>
+        <Stack
+          direction="row"
+          spacing={3}
+          alignItems="center"
+        >
+          <Task />
+          <Typography variant='h5' children="個人タスク" />
+        </Stack>
+        <Typography children="3つのタスクが残っています。" align='left' />
+        <PersonalTaskList />
+      </Stack>
+      <Footer />
+    </>
+  )
+}
