bc8db9326f1d42849ba47c7a8239ae90b18f53bc
mochi-yu <mochi.2225.it@gmail.com>
2023-06-19 01:24:23 +0900

    feat: 修正

--- src/FirebaseConfig.jsx
+++ src/FirebaseConfig.jsx
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCmtMDblq4tQ0UkH4nUpQPGS8ZUnVrY63w",
+  authDomain: "trend-to-do.firebaseapp.com",
+  projectId: "trend-to-do",
+  storageBucket: "trend-to-do.appspot.com",
+  messagingSenderId: "138744688273",
+  appId: "1:138744688273:web:e910122a0fa215a93a1930",
+  measurementId: "G-BMVHGQ51SS"
 };
 
 const app = initializeApp(firebaseConfig);
-console.log(import.meta.env.VITE_FIREBASE_API_KEY)
-console.log(import.meta.env.MODE)
