88c9ad1669515118108298df4abde0d37c3b5d71
Yudai Fujimoto <apricot4096@gmail.com>
2023-06-23 04:52:58 +0900

    Add axios setting

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import { Header } from './components/Header';
 import './App.css';
 import { RecoilRoot } from 'recoil';
 
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export default function App() {
 
   return (
