d3eb6ac9258d93b14b04fc7e3814bf47951a9df7
mochi-yu <mochi.2225.it@gmail.com>
2023-06-24 11:05:45 +0900

    fix: ロード画面の重なり順を修正

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import { Backdrop, CircularProgress } from "@mui/material"
 
 export const Loading = (props) => {
   return (
-    <Backdrop open={props.open}>
+    <Backdrop open={props.open} sx={{zIndex: 20}}>
       <CircularProgress color="inherit" />
     </Backdrop>
   )
